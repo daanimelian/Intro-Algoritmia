a8dd3cb59f4ead84c9659b4c73ca34a8286a1203
Daniela Melian <meliandaani@gmail.com>
2024-05-01 15:55:40 -0300

    ejercicios 8, 9 y 10

--- GUIA5/ejercicio_8.py
+++ GUIA5/ejercicio_8.py
@@ -10,3 +10,55 @@ informe que contenga:
 · Importe total de sueldos por cada categoría.
 · Salario promedio
 """
+total_salarios = 0
+empleados_mejores_pagos = 0
+empleados_peores_pagos = 0
+salarios_mas_bajo = 0
+salarios_categoria_1 = 0
+salarios_categoria_2 = 0
+salarios_categoria_3 = 0
+salario_prom = 0
+cant_empleados = 0
+legajo = 0
+mejor_sueldo = 0
+peor_sueldo = 0
+primer_empleado = True
+while legajo != -1:
+    legajo = int(input("Ingrese el numero de legajo del trabajador. Para finalizar ingrese -1: "))
+    if legajo != -1:
+        categoria = int(input("Ingrese la categoria del trabajador (1,2 o 3): "))
+        while categoria != 1 and categoria != 2 and categoria != 3:
+            categoria = int(input("Categoria incorrecta. Ingrese la categoria del trabajador (1,2 o 3): "))
+        salario = int(input("Ingrese el salario del trabajador: "))
+        while salario < 0:
+            salario = int(input("Salario incorrecto. Ingrese el salario del trabajador: "))
+        if salario > 200000:
+            empleados_mejores_pagos += 1
+        if salario < 50000 and categoria == 3:
+            empleados_peores_pagos += 1
+        if salario > mejor_sueldo:
+            legajo_mejor_sueldo = legajo
+            mejor_sueldo = salario
+        if salario < peor_sueldo or primer_empleado:
+            peor_sueldo = salario
+            primer_empleado = False
+        if categoria == 1:
+            salarios_categoria_1 += salario
+        elif categoria == 2:
+            salarios_categoria_2 += salario
+        elif categoria == 3:
+            salarios_categoria_3 += salario
+        cant_empleados += 1
+        total_salarios += salario
+
+if cant_empleados > 0:
+    salario_prom = total_salarios / cant_empleados
+
+print("· Importe total de salarios pagados por la empresa: $", total_salarios, "\n",
+      "· Cantidad de empleados que ganan más de $200000:", empleados_mejores_pagos, "\n",
+      "· Cantidad de empleados que ganan menos de $50000, cuya categoría sea 3:", empleados_peores_pagos, "\n",
+      "· Legajo del empleado que más gana: ", legajo_mejor_sueldo, "\n",
+      "· Sueldo más bajo: $", peor_sueldo, "\n",
+      "· Importe total de sueldos por cada categoría: Categoria 1 - $", salarios_categoria_1, "Categoria 2 - $",
+      salarios_categoria_2, "Categoria 3 - $", salarios_categoria_3, "\n",
+      "· Salario promedio: $", total_salarios, "\n")
--- GUIA5/ejercicio_9.py
+++ GUIA5/ejercicio_9.py
@@ -5,3 +5,18 @@ Ejemplo:
 · Si se ingresa 5, se debe mostrar 1 3 5 y la suma es 9.
 · Si se ingresa 8, se debe mostrar 1 3 5 7 y la suma es 16.
 · Si se ingresa -5, se debe pedir otro número."""
+
+numero = int(input("Ingresar un numero entero y positivo: "))
+suma = 0
+i = 1
+while numero < 0:
+    numero = int(input("Numero incorrecto. Ingresar un numero entero y positivo: "))
+
+while i <= numero:
+    if i % 2 != 0:
+        print(i)
+        suma += i
+
+    i += 1
+
+print("La suma es: ", suma)
