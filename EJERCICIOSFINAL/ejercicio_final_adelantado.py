abcc5493b057d21435ccd84a135f767a8e87ebaf
Daniela Melian <meliandaani@gmail.com>
2024-07-23 22:15:06 -0300

    ejercicio 5 de final

---
+++ EJERCICIOSFINAL/ejercicio_final_adelantado.py
@@ -0,0 +1,145 @@
+# Para un casino se solicita armar un programa que permita realizar n ejecuciones y por cada ejecución determine
+# entre 256 valores al azar que pueden ser números entre -8 y -2534, determinar cuál es el valor mayor encontrado.
+# Armar la función que permita ingresar los valores y otra que permita ingresar los rangos de valores, determinar
+# cuántos números pares e impares se obtuvieron e informarlos. Indicar cuales de los números pares son múltiplos de
+# 10 y que cantidad son. Luego solicitar un número dentro del rango de los pares y mostrar un mensaje que diga si
+# existe entre los valores ingresados, o un mensaje de error en caso de no hallar ese número. Este proceso deberá
+# realizarse mediante búsqueda binaria. También nos pidieron documentar el programa, indicando las funciones,
+# y pasos fundamentales para el funcionamiento del programa
+import pdb
+import random
+
+
+def definir_rangos_valores():
+    rango_inferior = -2534
+    while rango_inferior > -8 or rango_inferior < -2534:
+        rango_inferior = int(input("Rango inferior incorrecto. Ingrese el rango inferior: "))
+
+    rango_superior = -8
+    while rango_inferior > rango_superior or rango_superior > -8 or rango_superior < -2534:
+        rango_superior = int(input("Rango superior incorrecto. Ingrese el rango superior: "))
+
+    return rango_superior, rango_inferior
+
+
+def generar_valores_aleatorios():
+    lista_valores = []
+    rango_sup, rango_inf = definir_rangos_valores()
+    for i in range(256):
+        num_rand = random.randint(rango_inf, rango_sup)
+        lista_valores.append(num_rand)
+    return lista_valores
+
+
+def num_pares_impares(lista):
+    largo = len(lista)
+    num_pares = []
+    num_impares = []
+
+    for i in range(largo):
+        if lista[i] % 2 == 0:
+            num_pares.append(lista[i])
+        else:
+            num_impares.append(lista[i])
+    return num_pares, num_impares
+
+
+def valores_mult_de_10(lista):
+    largo = len(lista)
+    mult_10 = []
+    for i in range(largo):
+        if lista[i] % 2 == 0 and lista[i] % 5 == 0:
+            mult_10.append(lista[i])
+    return mult_10
+
+
+def rango_en_lista(lista):
+    rango_superior = 0
+    rango_inferior = 0
+    largo = len(lista)
+
+    for i in range(largo):
+        if i == 0:
+            rango_inferior = lista[i]
+            rango_superior = lista[i]
+        elif lista[i] < rango_inferior:
+            rango_inferior = lista[i]
+        elif lista[i] > rango_superior:
+            rango_superior = lista[i]
+    return rango_superior, rango_inferior
+
+
+def ordenar_lista(lista):
+    largo = len(lista)
+
+    for i in range(largo - 1):
+        for j in range(i + 1, largo):
+            if lista[i] > lista[j]:
+                aux = lista[i]
+                lista[i] = lista[j]
+                lista[j] = aux
+    return lista
+
+
+def busqueda_binaria(lista, elemento):
+    largo = len(lista)
+    izquierda = 0
+    derecha = largo - 1
+    posicion = -1
+
+    while izquierda <= derecha and posicion == -1:
+
+        centro = (izquierda + derecha) // 2
+        if elemento == lista[centro]:
+            posicion = centro
+        elif elemento > lista[centro]:
+            izquierda = centro + 1
+        else:
+            derecha = centro - 1
+
+    return posicion
+
+
+def main():
+    n = 2
+    while n < 0:
+        n = int(input("Numero incorrecto. Ingresar el numero de ejecuciones del programa: "))
+
+    for i in range(n):
+
+        lista_valores = generar_valores_aleatorios()
+        lista_valores_ordenada = ordenar_lista(lista_valores)
+        print("El mayor elemento encontrado de la lista es: ", lista_valores_ordenada[len(lista_valores_ordenada)-1])
+        num_pares, num_impares = num_pares_impares(lista_valores_ordenada)
+        num_mult_10 = valores_mult_de_10(num_pares)
+
+        print("Se encontraron ", len(num_pares), " numeros pares y son: ", end=" ")
+        for i in range(len(num_pares)):
+            print(num_pares[i], end=" ")
+
+        print("")
+
+        print("Se encontraron ", len(num_impares), " numeros impares y son: ", end=" ")
+        for i in range(len(num_impares)):
+            print(num_impares[i], end=" ")
+
+        print("")
+
+        print("Se encontraron ", len(num_mult_10), " numeros multiplos de 10 y son: ", end=" ")
+        for i in range(len(num_mult_10)):
+            print(num_mult_10[i], end=" ")
+
+        rango_sup_par, rango_inf_par = rango_en_lista(num_pares)
+        print("")
+        print("El rango de numeros pares es de ", rango_inf_par, " hasta ", rango_sup_par)
+        elemento = int(input("Ingrese un numero dentro del rango para saber si esta o no en la lista: "))
+        indice = busqueda_binaria(num_pares, elemento)
+        if indice == -1:
+            print("El numero no se encuentra dentro de la lista.")
+        else:
+            print("El numero se encuentra dentro de la lista!")
+        print(" ")
+        print(" ------------------------------------------------------------------------------------------- ")
+        print(" ")
+
+main()
---
+++ EJERCICIOSFINAL/ejercicio_tipo_final_1.py
@@ -0,0 +1,97 @@
+# Ingresar un número entero y positivo y generar la siguiente serie
+#
+# Si el valor es impar, multiplicarlo por 3 y sumarle 1; si el valor es par, dividirlo por 2.
+#
+# Evaluar el resultado obtenido e Ir repitiendo las operaciones, según corresponda, hasta que el valor final sea 1
+#
+# Por ejemplo, se ingresa el 7
+#
+# 7*3+1=**22**/2=**11***3+1=**34**/2=**17***3+1=**52**/2=**26**/2=**13***3+1=**40**/2=**20**/2=**10**/2=**5***3+1=**16**/2=**8**/2=**4**/2=**2**/2=**1**
+#
+# Se solicita:
+#
+# - Ir guardando los resultados obtenidos en una lista
+# - Informar cuantos valores se guardaron en la lista
+# - Informar si en la lista hay valores primos, y cuantos. Determinar si el valor es primo con una función
+# - Mostrar la lista original con los valores separados por 3 espacios
+# - Ordenar la lista de manera descendente y volverla a mostrar
+#
+# **Número primo:** Número divisible solamente por 1 y por si mismo. Ej 5
+
+lista_resultados = []
+
+
+def realizar_cuenta(numero):
+    if numero % 2 == 0:
+        numero = (numero / 2)
+    else:
+        numero = (numero * 3) + 1
+
+    return int(numero)
+
+
+def ordenar_lista(lista, asc=True):
+    largo = len(lista)
+
+    for i in range(largo - 1):
+        for j in range(i + 1, largo):
+            if asc is True:
+                if lista[i] > lista[j]:
+                    aux = lista[i]
+                    lista[i] = lista[j]
+                    lista[j] = aux
+            else:
+                if lista[i] < lista[j]:
+                    aux = lista[i]
+                    lista[i] = lista[j]
+                    lista[j] = aux
+
+    return lista
+
+
+def numeros_primos(lista):
+    largo = len(lista)
+    num_primo = []
+    for i in range(largo):
+        cant_divisores = 0
+        for j in range(1, lista[i]):
+            if lista[i] % j == 0:
+                cant_divisores += 1
+        if cant_divisores == 1:
+            num_primo.append(lista[i])
+    return num_primo
+
+
+n = int(input("Ingresar un numero entero y positivo: "))
+while n < 0:
+    n = int(input("El numero no es positivo. Ingresar un numero entero y positivo: "))
+
+while n > 1:
+
+    n = realizar_cuenta(n)
+    lista_resultados.append(n)
+num_primos = numeros_primos(lista_resultados)
+
+
+print("La lista de num es: ", len(lista_resultados), " y son: ", end=" ")
+for i in range(len(lista_resultados)):
+    print(lista_resultados[i], end=" ")
+print("")
+
+
+
+lista_ordenada = ordenar_lista(lista_resultados, asc=True)
+print("La lista de num ordenada asc es: ", len(lista_ordenada), " y son: ", end=" ")
+for i in range(len(lista_ordenada)):
+    print(lista_ordenada[i], end=" ")
+print("")
+
+lista_ordenada_des = ordenar_lista(lista_resultados, asc=False)
+print("La lista de num desc es: ", len(lista_ordenada_des), " y son: ", end=" ")
+for i in range(len(lista_ordenada_des)):
+    print(lista_ordenada_des[i], end=" ")
+
+print("")
+print("Se encontraron ", len(num_primos), " numeros primos y son: ", end=" ")
+for i in range(len(num_primos)):
+    print(num_primos[i], end=" ")
